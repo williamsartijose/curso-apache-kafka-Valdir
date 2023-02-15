0eb64de05a97b9291db6bdfa86aa67b2bc59f93f
unknown <williamsartijose@hotmail.com>
2023-02-15 18:47:25 +0000

    str-producer started

---
+++ str-producer/str-producer/src/test/java/com/william/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
